3505c7780c2d3a8be72f397030fa5c7052bdf5d6
Dennis Roxxx <dennisroxxx@MacBook-Air-Dennis.local>
2023-01-19 15:12:43 +0300

    done

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,8 +1,10 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         if (str == null) return false;
-        return !str.contains("-") && !str.equals("0") && !str.trim().equals("");
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
